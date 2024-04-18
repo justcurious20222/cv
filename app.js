ab5ac9adc11023d3a4a2cbfbf12904860b7e7403
justcurious20222 <167424452+justcurious20222@users.noreply.github.com>
2024-04-18 10:35:14 +0000

    Uploaded the Root

---
+++ app.js
@@ -0,0 +1,27 @@
+// function name(name){
+//     console.log(name); 
+// }
+// let name = "John";
+// let age = 30;
+
+// console.log(`Name: ${name}, Age: ${age}`);
+
+// console.log('hello kids'); 
+
+// let firstName = true ;  
+// console.log(typeof firstName); 
+
+// let person = {
+//     name : "idiot", 
+//     age : 38
+// }; 
+
+// let array = ["hello","ok"]; 
+// console.log(typeof array); 
+// alert('idiot'); 
+
+function idiot( name){
+    return name; 
+}
+
+console.log(idiot("idiot")); 
